6b0bd0167306104a1d74d826b4d59bbc80f95f63
N-os <natali-os@ukr.net>
2024-06-17 23:06:26 +0300

    task-2 done

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,21 @@
 'use strict';
+function formatMessage(message, maxLength) {
+  const messageLength = message.length;
+  let messageStandard;
+  if (messageLength <= maxLength) {
+    messageStandard = message;
+  } else {
+    messageStandard = message.slice(0, maxLength) + '...';
+  }
+  return messageStandard;
+}
+console.log(formatMessage('Curabitur ligula sapien', 16)); // "Curabitur ligula..."
+console.log(formatMessage('Curabitur ligula sapien', 23)); // "Curabitur ligula sapien"
+console.log(formatMessage('Vestibulum facilisis purus nec', 20)); // "Vestibulum facilisis..."
+console.log(formatMessage('Vestibulum facilisis purus nec', 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15)); // "Nunc sed turpis..."
+console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41)); // "Nunc sed turpis a felis in nunc fringilla"
+
+//=================================================================
+
+// messageLength <= maxLength ? (messageStandard = message) : (messageStandard = message.slice(0, maxLength) + '...');
